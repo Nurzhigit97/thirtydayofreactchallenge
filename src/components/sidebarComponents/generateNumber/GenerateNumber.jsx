c023b8d1a339293a4a7a7fbedc47a44e017e00a8
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-06-25 13:30:02 +0600

    Generate Number

--- src/components/sidebarComponents/generateNumber/GenerateNumber.jsx
+++ src/components/sidebarComponents/generateNumber/GenerateNumber.jsx
@@ -1,7 +1,33 @@
 import "./GenerateNumber.scss";
 
+const GenerateNum = () => {
+  let numbers = [];
+
+  for (let i = 0; i < 40; i++) {
+    numbers.push(i);
+  }
+  return (
+    <div className="numbers">
+      <div className="odd">
+        {numbers.map((num) => (
+          <h1 className="oddEven">{num}</h1>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const GenerateNumber = () => {
-  return <div>GenerateNumber</div>;
+  return (
+    <div className="generateNumber">
+      <h1 className="text-center">Generate Number</h1>
+      <div className="numbers">
+        <ul>
+          <GenerateNum />
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default GenerateNumber;
