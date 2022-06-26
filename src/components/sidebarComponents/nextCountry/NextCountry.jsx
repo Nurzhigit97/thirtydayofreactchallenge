dc4587e57bda9a00bbe48d1343ce2b4aec18d701
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-06-26 22:18:49 +0600

    added search

--- src/components/sidebarComponents/nextCountry/NextCountry.jsx
+++ src/components/sidebarComponents/nextCountry/NextCountry.jsx
@@ -5,7 +5,7 @@ import { Spinner } from "react-bootstrap";
 const NextCountry = () => {
   const [countries, setCountries] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [searchKey, setSearchKey] = useState("");
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
@@ -32,26 +32,39 @@ const NextCountry = () => {
   console.log(countries);
   return (
     <div className="nextCountries">
+      <input
+        value={searchKey}
+        onChange={(e) => {
+          setSearchKey(e.target.value);
+        }}
+        type="text"
+        className="form-control"
+        placeholder="Search items..."
+      />
       {loading ? (
         <div className="data">
-          {countries.map(
-            ({ name, flags, capital, languages, population, currencies }) => (
-              <div className="blockData">
-                <div className="imgBlock">
-                  <img src={flags.png} alt="flags" />
+          {countries
+            .filter(({ name }) => name.common.toLowerCase().includes(searchKey))
+            .map(
+              ({ name, flags, capital, languages, population, currencies }) => (
+                <div className="blockData">
+                  <div className="imgBlock">
+                    <img src={flags.png} alt="flags" />
+                  </div>
+                  <h4 className="text-center">{name.common}</h4>
+                  <p>Capital: {capital == null ? "not have" : capital}</p>
+                  <p>Надо получить языки из Объекта</p>
+                  <p>
+                    Population:
+                    {population
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    <i class="fa-solid fa-users"></i>
+                  </p>
+                  <p>Currency:{} </p>
                 </div>
-                <h4 className="text-center">{name.common}</h4>
-                <p>Capital: {capital == null ? "not have" : capital}</p>
-                <p>Надо получить языки из Объекта</p>
-                <p>
-                  Population:
-                  {population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                  <i class="fa-solid fa-users"></i>
-                </p>
-                <p>Currency:{} </p>
-              </div>
-            )
-          )}
+              )
+            )}
         </div>
       ) : (
         <div className="text-center">
