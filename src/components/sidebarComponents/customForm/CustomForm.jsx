c2c6fed6a8a04b44fcb67bebcec7dc2d77598dae
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-06-28 23:39:53 +0600

    perf(CustomForm): created custom Inputs

--- src/components/sidebarComponents/customForm/CustomForm.jsx
+++ src/components/sidebarComponents/customForm/CustomForm.jsx
@@ -1,6 +1,33 @@
 import "./CustomForm.scss";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, useField } from "formik";
 import * as Yup from "yup";
+
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.name}>{label}</label>
+      <input {...props} {...field} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+const MyCheckBox = ({ children, ...props }) => {
+  const [field, meta] = useField({ ...props, type: "checkbox" });
+  return (
+    <>
+      <label className="checkbox">
+        <input type="checkbox" {...props} {...field} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
 const CustomForm = () => {
   return (
@@ -19,7 +46,9 @@ const CustomForm = () => {
         email: Yup.string()
           .email("Неправильный email адрес!")
           .required("Обязательное поле"),
-        amount: Yup.number().min(5, "Не менее 5").required("Обязательное поле"),
+        amount: Yup.number()
+          .min(5, "Не менее 5")
+          .required("Обязательное поле!"),
         currency: Yup.string().required("Выберите валюту"),
         text: Yup.string().min(10, "Не менее 10 символов"),
         terms: Yup.boolean()
@@ -30,15 +59,14 @@ const CustomForm = () => {
     >
       <Form className="form">
         <h2>Отправить пожертвование</h2>
-        <label htmlFor="name">Ваше имя</label>
-        <Field id="name" name="name" type="text" />
-        <ErrorMessage className="error" name="name" component="div" />
-        <label htmlFor="email">Ваша почта</label>
-        <input id="email" name="email" type="email" />
-        <ErrorMessage className="error" name="email" component="div" />
-        <label htmlFor="amount">Количество</label>
-        <Field id="amount" name="amount" type="number" />
-        <ErrorMessage className="error" name="amount" component="div" />
+        <MyTextInput label="Ваше имя" id="name" name="name" type="text" />
+        <MyTextInput label="Ваша почта" id="email" name="email" type="email" />
+        <MyTextInput
+          label="Количество"
+          id="amount"
+          name="amount"
+          type="number"
+        />
         <label htmlFor="currency">Валюта</label>
         <Field id="currency" name="currency" as="select">
           <option value="">Выберите валюту</option>
@@ -50,11 +78,9 @@ const CustomForm = () => {
         <label htmlFor="text">Ваше сообщение</label>
         <Field id="text" name="text" as="textarea" />
         <ErrorMessage className="error" name="text" component="div" />
-        <label className="checkbox">
-          <Field name="terms" type="checkbox" />
+        <MyCheckBox name="terms">
           Соглашаетесь с политикой конфиденциальности?
-        </label>
-        <ErrorMessage className="error" name="terms" component="div" />
+        </MyCheckBox>
         <button type="submit">Отправить</button>
       </Form>
     </Formik>
