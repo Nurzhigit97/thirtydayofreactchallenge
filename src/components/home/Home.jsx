2b5aaadbd2bce1821b46c6f4b0630ab5e534f226
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-07-01 00:14:57 +0600

    perf(Home):create Component Home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h3 className="text-center mt-2">Welcome Friends to my WebApp</h3>
+      <h5>
+        My name's Nurzhigit i want be in my second life a Full-stack Developer
+      </h5>
+      <p>
+        This file have all React topic cheatsheets for to fast create WebApps
+      </p>
+    </div>
+  );
+};
+
+export default Home;
