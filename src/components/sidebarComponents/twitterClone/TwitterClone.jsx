747ddac9e986e8a4e36a3931a49c24b163111899
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-06-30 15:42:17 +0600

    perf(TwitterClone):done style .msgUsers

--- src/components/sidebarComponents/twitterClone/TwitterClone.jsx
+++ src/components/sidebarComponents/twitterClone/TwitterClone.jsx
@@ -1,5 +1,5 @@
 import "./TwitterClone.scss";
-
+import imgPostedUsers from "../../../assets/postUsers/IMG_20200906_105547.jpg";
 const TwitterClone = () => {
   return (
     <div className="twitterClone">
@@ -20,16 +20,19 @@ const TwitterClone = () => {
       <hr />
       <div className="msgUsers">
         <div className="msgUsersHeader">
-          <i class="fas fa-user"></i>
-          <h3>Nurzhigit Shaidiev</h3>
-          <p>@Nurzhigit</p>
+          <img src={imgPostedUsers} alt="" />
+          <h3 className="userName">Nurzhigit Shaidiev</h3>
+          <p>@shaidievN</p>
         </div>
         <div className="msgUsersMain">
-          <p className="p-3">
+          <p className="mainMsg p-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
             quis quibusdam culpa, quae ut quasi et est perspiciatis in expedita
             optio esse officia porro soluta! Consectetur quis adipisci quasi
-            quas?
+            quas?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Recusandae voluptatem incidunt doloremque. Consectetur, numquam modi
+            vitae nulla rem beatae ullam qui nostrum quo? Odio dolore autem
+            minus dolorem porro sit!
           </p>
         </div>
         <div className="msgUsersFooter">
@@ -50,16 +53,19 @@ const TwitterClone = () => {
       <hr />
       <div className="msgUsers">
         <div className="msgUsersHeader">
-          <i class="fas fa-user"></i>
-          <h3>Nurzhigit Shaidiev</h3>
-          <p>@Nurzhigit</p>
+          <img src={imgPostedUsers} alt="" />
+          <h3 className="userName">Nurzhigit Shaidiev</h3>
+          <p>@shaidievN</p>
         </div>
         <div className="msgUsersMain">
-          <p className="p-3">
+          <p className="mainMsg p-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
             quis quibusdam culpa, quae ut quasi et est perspiciatis in expedita
             optio esse officia porro soluta! Consectetur quis adipisci quasi
-            quas?
+            quas?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Recusandae voluptatem incidunt doloremque. Consectetur, numquam modi
+            vitae nulla rem beatae ullam qui nostrum quo? Odio dolore autem
+            minus dolorem porro sit!
           </p>
         </div>
         <div className="msgUsersFooter">
@@ -80,16 +86,19 @@ const TwitterClone = () => {
       <hr />
       <div className="msgUsers">
         <div className="msgUsersHeader">
-          <i class="fas fa-user"></i>
-          <h3>Nurzhigit Shaidiev</h3>
-          <p>@Nurzhigit</p>
+          <img src={imgPostedUsers} alt="" />
+          <h3 className="userName">Nurzhigit Shaidiev</h3>
+          <p>@shaidievN</p>
         </div>
         <div className="msgUsersMain">
-          <p className="p-3">
+          <p className="mainMsg p-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
             quis quibusdam culpa, quae ut quasi et est perspiciatis in expedita
             optio esse officia porro soluta! Consectetur quis adipisci quasi
-            quas?
+            quas?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Recusandae voluptatem incidunt doloremque. Consectetur, numquam modi
+            vitae nulla rem beatae ullam qui nostrum quo? Odio dolore autem
+            minus dolorem porro sit!
           </p>
         </div>
         <div className="msgUsersFooter">
