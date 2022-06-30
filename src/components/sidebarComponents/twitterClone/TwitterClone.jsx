d4c7248444540a285fe8f76efd84ec6f1c35a230
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-06-30 18:14:32 +0600

    perf(TwitterClone):add Func Show Hide and Slice message

--- src/components/sidebarComponents/twitterClone/TwitterClone.jsx
+++ src/components/sidebarComponents/twitterClone/TwitterClone.jsx
@@ -1,37 +1,70 @@
+import { useState } from "react";
 import "./TwitterClone.scss";
 import imgPostedUsers from "../../../assets/postUsers/IMG_20200906_105547.jpg";
 
-const TwitterClone = () => {
-  const GetDate = () => {
-    let date = new Date();
+const getDateUser = () => {
+  let date = new Date();
 
-    let month = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
+  let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    let monthStr = month[date.getMonth()];
-    let day = date.getDay();
-    let fullYear = date.getFullYear();
-    let hour = date.getHours();
-    let minutes = date.getMinutes();
+  let monthStr = month[date.getMonth()];
+  let day = date.getDay();
+  let fullYear = date.getFullYear();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
 
-    return `${monthStr.slice(0, 3)} ${
-      day < 10 ? `0${day}` : day
-    }, ${fullYear} ${hour < 10 ? `0${hour}` : hour}:${
-      minutes < 10 ? `0${minutes}` : minutes
-    } ${hour > 6 && hour < 18 ? "pm" : "am"}`;
+  return `${monthStr.slice(0, 3)} ${day < 10 ? `0${day}` : day}, ${fullYear} ${
+    hour < 10 ? `0${hour}` : hour
+  }:${minutes < 10 ? `0${minutes}` : minutes} ${
+    hour > 6 && hour < 18 ? "pm" : "am"
+  }`;
+};
+//For Post Msg
+const SliceMsg = ({ msg }) => {
+  return msg.replace(/\s/g, "").length > 250
+    ? `${msg.slice(0, 250)}.....`
+    : msg;
+};
+
+const ShowMgs = (msg) => {
+  let [showMsg, setShowMsg] = useState(false);
+
+  const handleClick = () => {
+    setShowMsg((current) => !current);
   };
+
+  return (
+    <div>
+      {showMsg ? <SliceMsg msg={msg} /> : msg}
+      <button onClick={handleClick} className="btn btn-success">
+        {!showMsg ? "Hide Msg" : "Show Msg"}
+      </button>
+    </div>
+  );
+};
+//For Post Msg
+const TwitterClone = () => {
+  let lorem = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+  quis quibusdam culpa, quae ut quasi et est perspiciatis in
+  expedita optio esse officia porro soluta! Consectetur quis
+  adipisci quasi quas?Lorem ipsum, dolor sit amet consectetur
+  adipisicing elit. Recusandae voluptatem incidunt doloremque.
+  Consectetur, numquam modi vitae nulla rem beatae ullam qui nostrum
+  quo? Odio dolore autem minus dolorem porro sit!`;
+
   return (
     <div className="twitterClone">
       <div className="twiiterClonePadding">
@@ -57,15 +90,7 @@ const TwitterClone = () => {
             <p>@shaidievN</p>
           </div>
           <div className="msgUsersMain">
-            <p className="mainMsg p-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              quis quibusdam culpa, quae ut quasi et est perspiciatis in
-              expedita optio esse officia porro soluta! Consectetur quis
-              adipisci quasi quas?Lorem ipsum, dolor sit amet consectetur
-              adipisicing elit. Recusandae voluptatem incidunt doloremque.
-              Consectetur, numquam modi vitae nulla rem beatae ullam qui nostrum
-              quo? Odio dolore autem minus dolorem porro sit!
-            </p>
+            <p className="mainMsg p-3">{ShowMgs(lorem)}</p>
           </div>
           <div className="msgUsersFooter">
             <div className="left">
@@ -78,7 +103,7 @@ const TwitterClone = () => {
               <i className="fa-solid fa-arrows-retweet"></i>
             </div>
             <div className="right">
-              <p className="text-dark">{GetDate()}</p>
+              <p className="text-dark">{getDateUser()}</p>
             </div>
           </div>
         </div>
@@ -111,7 +136,7 @@ const TwitterClone = () => {
               <i className="fa-solid fa-arrows-retweet"></i>
             </div>
             <div className="right">
-              <p className="text-dark">{GetDate()}</p>
+              <p className="text-dark">{getDateUser()}</p>
             </div>
           </div>
         </div>
@@ -144,7 +169,7 @@ const TwitterClone = () => {
               <i className="fa-solid fa-arrows-retweet"></i>
             </div>
             <div className="right">
-              <p className="text-dark">{GetDate()}</p>
+              <p className="text-dark">{getDateUser()}</p>
             </div>
           </div>
         </div>
