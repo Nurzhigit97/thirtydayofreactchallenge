0e4026275871e00b96a78780d412e2138cbfa478
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-07-01 00:24:31 +0600

    perf(SignIn):create SignIn Page

---
+++ src/layouts/login/Signin.jsx
@@ -0,0 +1,81 @@
+import { Link } from "react-router-dom";
+import "./login.scss";
+
+const Signin = () => {
+  return (
+    <div className="signup">
+      <div className="signLogin">
+        <Link to="/signin" className="signLoginLink">
+          Sign In
+        </Link>
+        <Link to="/login" className="signLoginLink">
+          Login
+        </Link>
+      </div>
+      <form className="auth">
+        <div className="form-group row">
+          <div className="col">
+            <label htmlFor="exampleInputFirstName1">First name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First name"
+            />
+          </div>
+          <div className="col">
+            <label htmlFor="exampleInputLastName1">Last name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last name"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword2">Confirm password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword2"
+            placeholder="Confirm password"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlFile1">Example file input</label>
+          <input
+            type="file"
+            className="form-control-file"
+            id="exampleFormControlFile1"
+          />
+        </div>
+        <button type="submit" className="text-center btnSignUp">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signin;
