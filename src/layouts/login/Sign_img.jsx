b5d2fc97203f434775ae39daaac9c74b9e1a39ed
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-07-04 22:32:00 +0600

    Component for Img Login on right side

---
+++ src/layouts/login/Sign_img.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import signImg from "../../assets/sign.svg";
+
+const Sign_img = () => {
+  return (
+    <div className="right_data mt-5" style={{ width: "100%" }}>
+      <div className="sign_img mt-5">
+        <img src={signImg} style={{ maxWidth: 480 }} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Sign_img;
