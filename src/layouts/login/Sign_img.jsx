7838abe6ebd5318d29fd30514f6c707b0db6f1f7
Nurzhigit97 <106031957+Nurzhigit97@users.noreply.github.com>
2022-07-04 23:06:07 +0600

    perf(Login):remove img after 990 width login in right side

--- src/layouts/login/Sign_img.jsx
+++ src/layouts/login/Sign_img.jsx
@@ -3,7 +3,7 @@ import signImg from "../../assets/sign.svg";
 
 const Sign_img = () => {
   return (
-    <div className="right_data mt-5" style={{ width: "100%" }}>
+    <div className="signImg right_data mt-5" style={{ width: "100%" }}>
       <div className="sign_img mt-5">
         <img src={signImg} style={{ maxWidth: 480 }} alt="" />
       </div>
